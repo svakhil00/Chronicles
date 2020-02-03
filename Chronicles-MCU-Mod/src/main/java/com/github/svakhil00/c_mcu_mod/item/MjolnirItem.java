0d7bfc2cd9604aa5ebd3ff21037017f1eecded01
svakhil00 <svakhil00@gmail.com>
2020-02-03 03:16:54 -0500

    Mjolnir now summons lightning

--- Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/item/MjolnirItem.java
+++ Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/item/MjolnirItem.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
@@ -20,6 +21,7 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult.Type;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
@@ -51,43 +53,53 @@ public class MjolnirItem extends TieredItem {
 					&& !state.isIn(BlockTags.LEAVES) && material != Material.GOURD ? 1.0F : 1.5F;
 		}
 	}
-	
+
 	public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
-	      stack.damageItem(1, attacker, (p_220045_0_) -> {
-	         p_220045_0_.sendBreakAnimation(EquipmentSlotType.MAINHAND);
-	      });
-	      return true;
-	   }
-	
+		stack.damageItem(1, attacker, (p_220045_0_) -> {
+			p_220045_0_.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+		});
+		return true;
+	}
+
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
-			ItemStack item = playerIn.getHeldItem(handIn);
-			Vec3d look = playerIn.getLookVec();
-			
-			LightningBoltEntity lightning = new LightningBoltEntity(worldIn, 1D, 1D, 1D, false);
-			lightning.setPosition(playerIn.lastTickPosX + look.x * 1.5D, playerIn.lastTickPosY + look.y * 1.5D, playerIn.lastTickPosZ + look.z * 1.5D);
+		ItemStack item = playerIn.getHeldItem(handIn);
+		Vec3d look = new Vec3d(0, 0, 0);
+		boolean block = false;
+		if (!playerIn.pick(100.0D, 1.0F, false).getType().equals(Type.MISS)) {
+			look = playerIn.pick(100.D, 1.0F, false).getHitVec();
+			block = true;
+		}
+		
+		if (block) {
+			LightningBoltEntity lightning = new LightningBoltEntity(worldIn, look.x, look.y, look.z, false);
 			lightning.setGlowing(true);
-			
-			((ServerWorld) worldIn).addLightningBolt(lightning);
-			return new ActionResult<ItemStack>(ActionResultType.SUCCESS, item);
-	   } 
+			if (!worldIn.isRemote) {
+				((ServerWorld) worldIn).addLightningBolt(lightning);
+			}
+		}
+		return new ActionResult<ItemStack>(ActionResultType.SUCCESS, item);
+	}
 
-	public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos, LivingEntity entityLiving) {
-	      if (state.getBlockHardness(worldIn, pos) != 0.0F) {
-	         stack.damageItem(2, entityLiving, (p_220044_0_) -> {
-	            p_220044_0_.sendBreakAnimation(EquipmentSlotType.MAINHAND);
-	         });
-	      }
+	public boolean onBlockDestroyed(ItemStack stack, World worldIn, BlockState state, BlockPos pos,
+			LivingEntity entityLiving) {
+		if (state.getBlockHardness(worldIn, pos) != 0.0F) {
+			stack.damageItem(2, entityLiving, (p_220044_0_) -> {
+				p_220044_0_.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+			});
+		}
 
-	      return true;
-	   }
-	
+		return true;
+	}
+
 	public Multimap<String, AttributeModifier> getAttributeModifiers(EquipmentSlotType equipmentSlot) {
-	      Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(equipmentSlot);
-	      if (equipmentSlot == EquipmentSlotType.MAINHAND) {
-	         multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER, "Weapon modifier", (double)this.ATTACKDAMAGE, AttributeModifier.Operation.ADDITION));
-	         multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", (double)this.ATTACKSPEED, AttributeModifier.Operation.ADDITION));
-	      }
+		Multimap<String, AttributeModifier> multimap = super.getAttributeModifiers(equipmentSlot);
+		if (equipmentSlot == EquipmentSlotType.MAINHAND) {
+			multimap.put(SharedMonsterAttributes.ATTACK_DAMAGE.getName(), new AttributeModifier(ATTACK_DAMAGE_MODIFIER,
+					"Weapon modifier", (double) this.ATTACKDAMAGE, AttributeModifier.Operation.ADDITION));
+			multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER,
+					"Weapon modifier", (double) this.ATTACKSPEED, AttributeModifier.Operation.ADDITION));
+		}
 
-	      return multimap;
-	   }
+		return multimap;
+	}
 }
