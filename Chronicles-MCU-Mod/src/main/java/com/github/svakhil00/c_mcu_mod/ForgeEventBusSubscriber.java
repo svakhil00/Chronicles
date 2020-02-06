45186dd20b95643b348482a4fd6a11b307ed7cb4
svakhil00 <svakhil00@gmail.com>
2020-02-06 05:51:16 -0500

    rename

--- Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/ForgeEventBusSubscriber.java
+++ Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/ForgeEventBusSubscriber.java
@@ -3,6 +3,6 @@ package com.github.svakhil00.c_mcu_mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @EventBusSubscriber(modid = Main.MODID, bus = EventBusSubscriber.Bus.FORGE)
-public class ModEventBusSubscriberForge {
+public class ForgeEventBusSubscriber {
 	
 }
