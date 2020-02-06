fd8bc347753c37eac16edb94c41fd292d4beaaf9
svakhil00 <svakhil00@gmail.com>
2020-02-06 05:51:26 -0500

    reorganize

--- Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/lists/ItemList.java
+++ Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/lists/ItemList.java
@@ -3,17 +3,24 @@ package com.github.svakhil00.c_mcu_mod.lists;
 import com.github.svakhil00.c_mcu_mod.init.ModItemGroups;
 import com.github.svakhil00.c_mcu_mod.item.CaptainShieldItem;
 import com.github.svakhil00.c_mcu_mod.item.MjolnirItem;
+
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.SpawnEggItem;
 
 public class ItemList {
+	//Weapons
 	public static final Item CAPTAIN_AMERICA_SHIELD = new CaptainShieldItem(ItemTierMaterialList.shield,11, -2.5f, new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).maxStackSize(1));
+	public static final Item MJOLNIR = new MjolnirItem(ItemTierMaterialList.mjolnir, -1, -3.0f,new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).maxStackSize(1));
+	
+	//blocks, ores
 	public static final BlockItem VIBRANIUM_ORE_BLOCKITEM = new BlockItem(BlockList.VIBRANIUM_ORE_BLOCK, new BlockItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
 	public static final Item VIBRANIUM_INGOT = new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
-	public static final Item MJOLNIR = new MjolnirItem(ItemTierMaterialList.mjolnir, -1, -3.0f,new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).maxStackSize(1));
 	public static final Item PURE_VIBRANIUM = new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
 	
-	
+	//entity eggs
+	//SpawnEggItem item = new SpawnEggItem(EntityType<?>, color1, color2, name);
 	
 
 }
