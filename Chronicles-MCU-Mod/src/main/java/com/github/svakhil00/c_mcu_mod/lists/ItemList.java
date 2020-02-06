a16039a91579fcdbbbff9fd13da3c4fbae37a998
svakhil00 <svakhil00@gmail.com>
2020-02-06 02:57:48 -0500

    anvil changes

--- Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/lists/ItemList.java
+++ Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/lists/ItemList.java
@@ -1,15 +1,10 @@
 package com.github.svakhil00.c_mcu_mod.lists;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 import com.github.svakhil00.c_mcu_mod.init.ModItemGroups;
 import com.github.svakhil00.c_mcu_mod.item.CaptainShieldItem;
 import com.github.svakhil00.c_mcu_mod.item.MjolnirItem;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.Item.Properties;
 
 public class ItemList {
 	public static final Item CAPTAIN_AMERICA_SHIELD = new CaptainShieldItem(ItemTierMaterialList.shield,11, -3.0f, new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).maxStackSize(1));
