388650d053351e023bfdfd7b1275a1cf63cd2d83
svakhil00 <svakhil00@gmail.com>
2020-02-08 03:00:57 -0500

    test

--- Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/ForgeEventBusSubscriber.java
+++ Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/ForgeEventBusSubscriber.java
@@ -1,8 +1,22 @@
 package com.github.svakhil00.c_mcu_mod;
 
+
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.EntityStruckByLightningEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
 @EventBusSubscriber(modid = Main.MODID, bus = EventBusSubscriber.Bus.FORGE)
 public class ForgeEventBusSubscriber {
+	
+	public static void lightning(EntityStruckByLightningEvent event) {
+		if(event.getEntity() instanceof PlayerEntity) {
+			//do something
+		}
+		
+		
+	}
+	
 	
 }
