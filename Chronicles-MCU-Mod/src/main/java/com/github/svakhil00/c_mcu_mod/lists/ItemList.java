4e17e1416078e419c655a3e97d93153233000c84
svakhil00 <svakhil00@gmail.com>
2020-02-23 18:44:24 -0500

    new item

--- Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/lists/ItemList.java
+++ Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/lists/ItemList.java
@@ -5,7 +5,9 @@ import com.github.svakhil00.c_mcu_mod.item.CaptainAmericaSuitItem;
 import com.github.svakhil00.c_mcu_mod.item.CaptainShieldItem;
 import com.github.svakhil00.c_mcu_mod.item.IronManSuitItem;
 import com.github.svakhil00.c_mcu_mod.item.MjolnirItem;
+import com.github.svakhil00.c_mcu_mod.item.PureVibraniumItem;
 import com.github.svakhil00.c_mcu_mod.item.ThorSuitItem;
+import com.github.svakhil00.c_mcu_mod.item.VibraniumOreBlockItem;
 
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.BlockItem;
@@ -18,9 +20,9 @@ public class ItemList {
 	public static final Item MJOLNIR = new MjolnirItem(ItemTierMaterialList.mjolnir, -1, -3.0f,new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP).maxStackSize(1));
 	
 	//blocks, ores
-	public static final BlockItem VIBRANIUM_ORE_BLOCKITEM = new BlockItem(BlockList.VIBRANIUM_ORE_BLOCK, new BlockItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
+	public static final BlockItem VIBRANIUM_ORE_BLOCKITEM = new VibraniumOreBlockItem(BlockList.VIBRANIUM_ORE_BLOCK, new BlockItem.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
 	public static final Item VIBRANIUM_INGOT = new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
-	public static final Item PURE_VIBRANIUM = new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
+	public static final Item PURE_VIBRANIUM = new PureVibraniumItem(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
 	public static final Item URU = new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
 	public static final Item STEEL =  new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
 	public static final Item ASGARDIAN_STEEL = new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP));
