c10b3fc2d6204a38a3f32e7887f1237ecb16e3b3
svakhil00 <svakhil00@gmail.com>
2020-02-25 12:04:42 -0500

    fixed random heals

--- Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/item/ThorChestplateItem.java
+++ Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/item/ThorChestplateItem.java
@@ -12,35 +12,32 @@ import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.world.World;
 
-public class CaptainAmericaChestplateItem extends CaptainAmericaSuitItem {
+public class ThorChestplateItem extends ThorSuitItem {
 
-	public CaptainAmericaChestplateItem(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builder) {
+	public ThorChestplateItem(IArmorMaterial materialIn, EquipmentSlotType slot, Properties builder) {
 		super(materialIn, slot, builder);
+		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
 		if (entityIn instanceof PlayerEntity) {
 			PlayerEntity playerEntity = (PlayerEntity) entityIn;
-			if (playerEntity.getItemStackFromSlot(EquipmentSlotType.CHEST)
-					.getItem() == ModItems.CAPTAIN_AMERICA_CHESTPLATE) {
-				if (playerEntity.getItemStackFromSlot(EquipmentSlotType.HEAD)
-						.getItem() == ModItems.CAPTAIN_AMERICA_HELMET) {
-					if (playerEntity.getItemStackFromSlot(EquipmentSlotType.LEGS)
-							.getItem() == ModItems.CAPTAIN_AMERICA_LEGGINGS) {
+			if (playerEntity.getItemStackFromSlot(EquipmentSlotType.CHEST).getItem() == ModItems.THOR_CHESTPLATE) {
+				if (playerEntity.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem() == ModItems.THOR_HELMET) {
+					if (playerEntity.getItemStackFromSlot(EquipmentSlotType.LEGS).getItem() == ModItems.THOR_LEGGINGS) {
 						if (playerEntity.getItemStackFromSlot(EquipmentSlotType.FEET)
-								.getItem() == ModItems.CAPTAIN_AMERICA_BOOTS) {
+								.getItem() == ModItems.THOR_BOOTS) {
 							if (!worldIn.isRemote) {
 								CompoundNBT tag = new CompoundNBT();
 								tag = stack.getOrCreateTag();
-								playerEntity.addPotionEffect(new EffectInstance(Effects.SPEED, 400, 2));
-								playerEntity.addPotionEffect(new EffectInstance(Effects.STRENGTH, 400, 0));
-								playerEntity.addPotionEffect(new EffectInstance(Effects.JUMP_BOOST, 400, 1));
+								playerEntity.addPotionEffect(new EffectInstance(Effects.RESISTANCE, 400, 1));
+								playerEntity.addPotionEffect(new EffectInstance(Effects.STRENGTH, 400, 1));
 								if (!playerEntity.isPotionActive(Effects.HEALTH_BOOST)) {
-									playerEntity.addPotionEffect(new EffectInstance(Effects.HEALTH_BOOST, 400, 2));
+									playerEntity.addPotionEffect(new EffectInstance(Effects.HEALTH_BOOST, 400, 3));
 									float health = tag.getFloat("heal");
 									if (health == 0) {
-										playerEntity.heal(12);
+										playerEntity.heal(16);
 									} else {
 										playerEntity.setHealth(health);
 									}
@@ -53,5 +50,4 @@ public class CaptainAmericaChestplateItem extends CaptainAmericaSuitItem {
 			}
 		}
 	}
-
 }
