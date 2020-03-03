5ffef98b377ea059ac109cc20ee12ee4dc454008
svakhil00 <svakhil00@gmail.com>
2020-03-03 03:09:46 -0500

    remove unused imports

--- Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/Main.java
+++ Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/Main.java
@@ -1,13 +1,5 @@
 package com.github.svakhil00.c_mcu_mod;
 
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig.Type;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.fml.loading.FMLPaths;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -15,6 +7,12 @@ import com.github.svakhil00.c_mcu_mod.config.Config;
 import com.github.svakhil00.c_mcu_mod.init.ModBlocks;
 import com.github.svakhil00.c_mcu_mod.init.ModEntityTypes;
 import com.github.svakhil00.c_mcu_mod.init.ModItems;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig.Type;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Main.MODID)
