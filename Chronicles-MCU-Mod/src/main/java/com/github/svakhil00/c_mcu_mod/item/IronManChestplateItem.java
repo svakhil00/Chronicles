8ad72605512e4dc71a8a989d305743e229cea082
svakhil00 <svakhil00@gmail.com>
2020-03-04 21:43:45 -0500

    fire resistance added

--- Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/item/IronManChestplateItem.java
+++ Chronicles-MCU-Mod/src/main/java/com/github/svakhil00/c_mcu_mod/item/IronManChestplateItem.java
@@ -7,6 +7,8 @@ import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.world.World;
 
 public class IronManChestplateItem extends IronManSuitItem {
@@ -18,13 +20,26 @@ public class IronManChestplateItem extends IronManSuitItem {
 
 	@Override
 	public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+		if (player.getItemStackFromSlot(EquipmentSlotType.HEAD).getItem() == ModItems.IRON_MAN_HELMET.get()) {
+			if (player.getItemStackFromSlot(EquipmentSlotType.LEGS).getItem() == ModItems.IRON_MAN_LEGGINGS
+					.get()) {
+				if (player.getItemStackFromSlot(EquipmentSlotType.FEET).getItem() == ModItems.IRON_MAN_BOOTS
+						.get()) {
+					if (!world.isRemote) {
+						player.addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, 400, 0));
+					}
+				}
+			}
+		}
+		
+		
+		
+		
 		CompoundNBT tag = stack.getOrCreateTag();
 		int energy = tag.getInt("energy");
-		
-		if(energy < 3999) {
-			energy += 2;
-		}else {
-			energy = 4000;
+
+		if (energy < 999) {
+			energy += 1;
 		}
 		tag.putInt("energy", energy);
 	}
